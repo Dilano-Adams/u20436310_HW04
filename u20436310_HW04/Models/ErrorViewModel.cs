4e134ee5d311fc3b49640eb28a5e0b9985f3c28b
Dilano-Adams <102999633+Dilano-Adams@users.noreply.github.com>
2022-08-11 11:26:58 +0200

    Add project files.

---
+++ u20436310_HW04/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace u20436310_HW04.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
